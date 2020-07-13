3f2836723d9befea9dfe946bc2a05819f293919a
Gustavo Vilas Boas <gustavovbs98@gmail.com>
2020-07-13 20:36:08 -0300

    Projeto criado

---
+++ Shield Nuzlocke/Shield Nuzlocke/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shield Nuzlocke
+//
+//  Created by Gustavo Vilas Boas on 13/07/20.
+//  Copyright © 2020 Gustavo Vilas Boas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
